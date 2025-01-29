001302ed808c47fa279565347594871358ddb08b
Juliana Salomé Escobar Campaña <107556745+JulianaEscobar31@users.noreply.github.com>
2025-01-28 22:11:25 -0500

    creación del proyecto

---
+++ repaso/src/test/java/ec/edu/espe/repaso/RepasoApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.repaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
